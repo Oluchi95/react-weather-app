9d6597178245e5da470d60b3119788b7ccbd94b2
Oluchi95 <ikeripriscilla94@gmail.com>
2024-04-26 16:55:57 +0100

    completed my project

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Lisbon" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodesfoundation.org/candidates/priscilla-ikeri-nigerian"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Priscilla Ikeri
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Oluchi95/weather-app-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://weather-app-react-ei8g.onrender.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
